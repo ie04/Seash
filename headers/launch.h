5e5848c4dd507177bcf843e2486e317180269e59
ie04 <iyadeltifi12@gmail.com>
2020-07-13 13:52:01 -0400

    Added the launch function at launch.h to execute programs

---
+++ headers/launch.h
@@ -0,0 +1,24 @@
+#ifndef LAUNCH_H
+#define LAUNCH_H 
+int seash_launch(char **args)
+{
+	pid_t pid, wpid
+	int status;
+
+	pid = fork();
+	if(pid == 0) { //Is child
+		if(execvp(args[0], args) == -1) { //Execs arg and checks for errors
+			perror("seash"); //Prints error with program name provided
+		}
+		exit(EXIT_FAILIURE);
+	} else if(pid < 0) { //Fork failiure
+		perror("seash");
+	} else { //Is parent
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED); //Waits for child status to change
+		} while(!WIFEXITED(status && !WIFSIGNALED(status)); //Loops waitpid until process is exited or killed
+	}
+
+	return 1;
+}
+#endif /* LAUNCH_H */
